16f4f16a9730a50e28194a41b0b35ac609efaf5c
Thanalakshan <bredmendon@gmail.com>
2024-06-29 19:49:37 +0000

    deleted:    model/hebbal_outliers.png 	deleted:    model/rajaji_nagar_outliers.png 	new file:   server/__pycache__/server.cpython-312.pyc 	new file:   server/__pycache__/util.cpython-312.pyc 	modified:   server/server.py 	modified:   server/util.py

--- server/util.py
+++ server/util.py
@@ -6,7 +6,7 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,bhk,bath):
+def get_estimated_price(location, sqft, bhk, bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -16,20 +16,19 @@ def get_estimated_price(location,sqft,bhk,bath):
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index>=0:
+    if loc_index >= 0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0],2)
-
+    return round(__model.predict([x])[0], 2)
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
-    global  __data_columns
+    global __data_columns
     global __locations
 
     with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+        __locations = __data_columns[3:]
 
     global __model
     if __model is None:
@@ -46,7 +45,7 @@ def get_data_columns():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))  # other location
     print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
